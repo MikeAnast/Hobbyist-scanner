c0b709ed02241b8a4ae5ec978c9b89bcb382fc3a
MikeAnast <manastas93@gmail.com>
2020-06-29 06:52:03 -0400

    V0.2 multithread simple scanner

--- simplescanner.py
+++ simplescanner.py
@@ -1,71 +1,75 @@
 
 import socket
 import threading
-import Queue
+import queue
 
 
-#print "Target to scan: "
-#targetIP = raw_input("> ")
-#print "Number of threads: "
-#threads = int(raw_input("> "))
 
-#target = "https://www.hackthissite.org"
-#target = 'pythonprogramming.net'
+target = input("Target to scan ->")
+#target = 'hackthissite.org'
+Nports= int(input("Number of ports ->"))
+
+
+
 
 def portscan(target,port):
-   sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-   sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)                        
-   sock.settimeout(10)
-   try:
-     con = sock.connect((target,port))
-     print("port{} is open".format(port))
-     con.close()
-   except:
-     return 0
+  try:
+    sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    sock.settimeout(10)
+    con = sock.connect((target,port))
+    #print("port{} is open".format(port))
+    #con.close()
+    return True
+  except:
+    return False
 
 
 def port_worker():
     while not q.empty():
         port = q.get()
         if portscan(target,port):
-            print("port {} is open".format(port))
             open_ports.append(port)
         else:
-            print("port {} is closed".format(port))
-         
-
-def threader(q):
-    while True:
-        worker = q.get()
-        port_worker()
-        q.task_done()
+            closed_ports.append(port)
 
 
 
 #ip = socket.gethostbyname(target)
 #print(ip)
 if __name__ == "__main__":
-   target = 'hackthissite.org'
-   Nthreads=100
-   print(target)
-   threads = []
-   open_ports = []
-
-   q = Queue.Queue()
+  
+  print(target)
+  threads = []
+  open_ports = []
+  closed_ports = []
 
 
-   for port in range(1,100):
-      q.put(port)
+  q = queue.Queue()
 
-   for i in range(Nthreads):
-      t = threading.Thread(name = "Thread id-"+str(i),target=threader,args=(q,))
-      threads.append(t)
-      t.start()
-      threads.append(t)
 
-   for thread in threads:
-      thread.join()
-   
+  for port in range(1,Nports):
+    q.put(port)
+
+  for i in range(Nports):
+    t = threading.Thread(target=port_worker)
+    threads.append(t)
+
+
+
+  for t in threads:
+    t.start()
+
+  for t in threads:
+    t.join()
+
+  print('open ports',open_ports)
+  print('closed ports',closed_ports)
+
+
+
+
+
 
 
 
